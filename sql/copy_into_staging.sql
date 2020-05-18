2b90bcca2a396ab42a03dd4c3b1cb70fedbe9059
praveen0584 <44709419+praveen0584@users.noreply.github.com>
2020-05-18 09:33:19 -0400

    Supply Chain Usescase
    Added cloud formation script, python scripts and sql scripts to the Supply Chain folder. Also has the steps documented.

---
+++ sql/copy_into_staging.sql
@@ -0,0 +1,12 @@
+SET query_group TO 'ingest';
+
+CREATE TEMPORARY TABLE nyc_greentaxi_tmp (LIKE supplychain.nyc_greentaxi);
+
+COPY nyc_greentaxi_tmp FROM '{0}' IAM_ROLE '{1}' csv ignoreheader 1 region '{2}' gzip;
+
+DELETE FROM supplychain.nyc_greentaxi USING nyc_greentaxi_tmp WHERE supplychain.nyc_greentaxi.vendorid = nyc_greentaxi_tmp.vendorid 
+AND supplychain.nyc_greentaxi.lpep_pickup_datetime = nyc_greentaxi_tmp.lpep_pickup_datetime;
+
+INSERT INTO supplychain.nyc_greentaxi SELECT * FROM nyc_greentaxi_tmp;
+
+DROP TABLE nyc_greentaxi_tmp;
